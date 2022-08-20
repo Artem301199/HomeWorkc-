9497b1097e30f067a3a0be164f168f351abda968
Artem <artem-vishnevskiy-00@mail.ru>
2022-08-20 15:31:56 +0300

    задача 4

---
+++ homework__4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите ваше число");
+int n = Convert.ToInt32(Console.ReadLine());
+int c = 1;
+
+while (c < n)
+{
+    if (c % 2 == 0)
+    {
+        Console.Write ( c + " " );
+        
+     }
+    
+    
+        c ++ ;
+    
+}
+Console.WriteLine( " " );
+
